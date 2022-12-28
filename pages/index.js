2f3794d02ee566266651ea4dd8b223dd7d5797ad
Alexei Garban <garban.valdeon@gmail.com>
2022-12-28 19:41:49 +0100

    text change

--- pages/index.js
+++ pages/index.js
@@ -41,15 +41,16 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
+          {/* <Image
             className={styles.logo}
             src="/next.svg"
             alt="Next.js Logo"
             width={180}
             height={37}
             priority
-          />
-          <div className={styles.thirteen}>
+          /> */}
+          SurveyJs Test Page
+          {/* <div className={styles.thirteen}>
             <Image
               src="/thirteen.svg"
               alt="13"
@@ -57,7 +58,7 @@ export default function Home() {
               height={31}
               priority
             />
-          </div>
+          </div> */}
         </div>
 
         <div className={styles.grid}>
